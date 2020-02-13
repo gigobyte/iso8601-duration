89f129d359a8b25c1136f670a18daed50b78b7db
Stanislav Iliev <gigobest2@gmail.com>
2020-02-13 23:39:08 +0200

    Update Duration.elm

--- src/Iso8601/Duration.elm
+++ src/Iso8601/Duration.elm
@@ -1,4 +1,4 @@
-module Iso8601.Duration exposing (Duration, duration)
+module Iso8601.Duration exposing (Duration)
 
 import Parser exposing ((|.), (|=), Parser, backtrackable, float, oneOf, succeed, symbol)
 
@@ -13,61 +13,13 @@ type alias Duration =
     }
 
 
-duration : Parser Duration
-duration =
-    succeed Duration
-        |. period
-        |= oneOf [ backtrackable years, succeed 0 ]
-        |= oneOf [ backtrackable months, succeed 0 ]
-        |= oneOf [ backtrackable daysFromWeek, days ]
-        |. oneOf [ timeDesignator, succeed () ]
-        |= oneOf [ backtrackable hours, succeed 0 ]
-        |= oneOf [ backtrackable minutes, succeed 0 ]
-        |= oneOf [ backtrackable seconds, succeed 0 ]
+type alias DurationComponents =
+    { period : String
+    , time : String
+    }
 
 
-period : Parser ()
-period =
-    symbol "P"
-
-
-years : Parser Float
-years =
-    float |. symbol "Y"
-
-
-months : Parser Float
-months =
-    float |. symbol "M"
-
-
-daysFromWeek : Parser Float
-daysFromWeek =
-    float
-        |. symbol "W"
-        |> Parser.map ((*) 7)
-
-
-days : Parser Float
-days =
-    float |. symbol "D"
-
-
-timeDesignator : Parser ()
-timeDesignator =
-    symbol "T"
-
-
-hours : Parser Float
-hours =
-    float |. symbol "H"
-
-
-minutes : Parser Float
-minutes =
-    float |. symbol "M"
-
-
-seconds : Parser Float
-seconds =
-    float |. symbol "S"
+componentsParser : Parser DurationComponents
+componentsParser =
+    oneOf
+        []
