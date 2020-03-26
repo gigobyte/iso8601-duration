e3c6ba06d64294563bdc4ef55c793a2394b68f6c
Stanislav Iliev <gigobest2@gmail.com>
2020-03-27 01:38:52 +0200

    Implement parser

--- src/Iso8601/Duration.elm
+++ src/Iso8601/Duration.elm
@@ -1,6 +1,6 @@
-module Iso8601.Duration exposing (Duration)
+module Iso8601.Duration exposing (Duration, fromString, parseDateElements, parseTimeElements)
 
-import Parser exposing ((|.), (|=), Parser, backtrackable, float, oneOf, succeed, symbol)
+import Parser exposing ((|.), (|=), Parser, Step(..), end, float, loop, oneOf, problem, succeed, symbol)
 
 
 type alias Duration =
@@ -13,13 +13,256 @@ type alias Duration =
     }
 
 
-type alias DurationComponents =
-    { period : String
-    , time : String
+initialDuration : Duration
+initialDuration =
+    { years = 0
+    , months = 0
+    , days = 0
+    , hours = 0
+    , minutes = 0
+    , seconds = 0
     }
 
 
-componentsParser : Parser DurationComponents
-componentsParser =
+type WeekElement
+    = Weeks Float
+
+
+type DateElement
+    = Years Float
+    | Months Float
+    | Days Float
+
+
+type TimeElement
+    = Hours Float
+    | Minutes Float
+    | Seconds Float
+
+
+fromString : String -> Maybe Duration
+fromString duration =
+    if not (String.startsWith "P" duration) then
+        Nothing
+
+    else if String.contains "W" duration then
+        -- PnW
+        case Parser.run weekParser duration of
+            Ok (Weeks weeks) ->
+                Just { initialDuration | days = weeks * 7 }
+
+            Err _ ->
+                Nothing
+
+    else
+        -- PnYnMnDTnHnMnS
+        case String.split "T" (String.replace "," "." duration) of
+            -- At least one element must be present, thus "P" is not a valid representation for a duration of 0 seconds
+            [ "P", "" ] ->
+                Nothing
+
+            [ "P" ] ->
+                Nothing
+
+            [ date, time ] ->
+                let
+                    dateElements =
+                        parseDateElements date
+
+                    timeElements =
+                        parseTimeElements time
+                in
+                case ( dateElements, timeElements ) of
+                    ( Just [], Just [] ) ->
+                        Nothing
+
+                    _ ->
+                        dateElements
+                            |> Maybe.map (addDateElementsToDuration initialDuration)
+                            |> (\intermediateDuration -> Maybe.map2 addTimeElementsToDuration intermediateDuration timeElements)
+
+            [ date ] ->
+                let
+                    dateElements =
+                        parseDateElements date
+                in
+                case dateElements of
+                    Just [] ->
+                        Nothing
+
+                    _ ->
+                        dateElements
+                            |> Maybe.map (addDateElementsToDuration initialDuration)
+
+            _ ->
+                Nothing
+
+
+parseDateElements : String -> Maybe (List DateElement)
+parseDateElements date =
+    Result.toMaybe (Parser.run dateElementsParser date)
+        |> Maybe.andThen
+            (\elements ->
+                if isOutOfOrder isValidDateElementSequence elements then
+                    Nothing
+
+                else
+                    Just elements
+            )
+
+
+parseTimeElements : String -> Maybe (List TimeElement)
+parseTimeElements date =
+    Result.toMaybe (Parser.run timeElementsParser date)
+        |> Maybe.andThen
+            (\elements ->
+                if isOutOfOrder isValidTimeElementSequence elements then
+                    Nothing
+
+                else
+                    Just elements
+            )
+
+
+addTimeElementsToDuration : Duration -> List TimeElement -> Duration
+addTimeElementsToDuration =
+    let
+        addTimeElementToDuration : TimeElement -> Duration -> Duration
+        addTimeElementToDuration element intermediateDuration =
+            case element of
+                Hours h ->
+                    { intermediateDuration | hours = h }
+
+                Minutes m ->
+                    { intermediateDuration | minutes = m }
+
+                Seconds s ->
+                    { intermediateDuration | seconds = s }
+    in
+    List.foldr addTimeElementToDuration
+
+
+addDateElementsToDuration : Duration -> List DateElement -> Duration
+addDateElementsToDuration =
+    let
+        addDateElementToDuration : DateElement -> Duration -> Duration
+        addDateElementToDuration element intermediateDuration =
+            case element of
+                Years y ->
+                    { intermediateDuration | years = y }
+
+                Months m ->
+                    { intermediateDuration | months = m }
+
+                Days d ->
+                    { intermediateDuration | days = d }
+    in
+    List.foldr addDateElementToDuration
+
+
+
+-- Parsers
+
+
+weekParser : Parser WeekElement
+weekParser =
+    succeed Weeks
+        |. symbol "P"
+        |= float
+        |. symbol "W"
+        |. end
+
+
+dateElementParser : List DateElement -> Parser (Step (List DateElement) (List DateElement))
+dateElementParser elements =
     oneOf
-        []
+        [ succeed (\a b -> Loop (b a :: elements))
+            |= float
+            |= oneOf
+                [ succeed Years |. symbol "Y"
+                , succeed Months |. symbol "M"
+                , succeed Days |. symbol "D"
+                ]
+        , succeed (Done elements)
+        ]
+
+
+dateElementsParser : Parser (List DateElement)
+dateElementsParser =
+    succeed identity
+        |. symbol "P"
+        |= loop [] dateElementParser
+
+
+timeElementParser : List TimeElement -> Parser (Step (List TimeElement) (List TimeElement))
+timeElementParser elements =
+    oneOf
+        [ succeed (\a b -> Loop (b a :: elements))
+            |= float
+            |= oneOf
+                [ succeed Hours |. symbol "H"
+                , succeed Minutes |. symbol "M"
+                , succeed Seconds |. symbol "S"
+                ]
+        , succeed (Done elements)
+        ]
+
+
+timeElementsParser : Parser (List TimeElement)
+timeElementsParser =
+    loop [] timeElementParser
+
+
+isValidTimeElementSequence : TimeElement -> TimeElement -> Bool
+isValidTimeElementSequence a b =
+    case ( a, b ) of
+        ( Minutes _, Hours _ ) ->
+            True
+
+        ( Seconds _, Minutes _ ) ->
+            True
+
+        ( Seconds _, Hours _ ) ->
+            True
+
+        _ ->
+            False
+
+
+isValidDateElementSequence : DateElement -> DateElement -> Bool
+isValidDateElementSequence a b =
+    case ( a, b ) of
+        ( Months _, Years _ ) ->
+            True
+
+        ( Days _, Months _ ) ->
+            True
+
+        ( Days _, Years _ ) ->
+            True
+
+        _ ->
+            False
+
+
+
+-- List util
+
+
+isOutOfOrder : (a -> a -> Bool) -> List a -> Bool
+isOutOfOrder compare elements =
+    let
+        isOutOfOrderHelper : List a -> Bool
+        isOutOfOrderHelper xs =
+            case xs of
+                x :: y :: others ->
+                    if not (compare x y) then
+                        True
+
+                    else
+                        isOutOfOrderHelper (y :: others)
+
+                _ ->
+                    False
+    in
+    isOutOfOrderHelper elements
--- tests/Duration.elm
+++ tests/Duration.elm
@@ -1,20 +1,33 @@
 module Duration exposing (suite)
 
 import Expect exposing (..)
-import Iso8601.Duration exposing (duration)
-import Parser
+import Iso8601.Duration exposing (fromString)
 import Test exposing (..)
 
 
 suite : Test
 suite =
     describe "Duration parsing"
-        [ test "P3Y6M4DT12H30M5S" <|
+        [ test "P23DT23H" <|
+            \_ ->
+                "P23DT23H"
+                    |> fromString
+                    |> Expect.equal
+                        (Just
+                            { years = 0
+                            , months = 0
+                            , days = 23
+                            , hours = 23
+                            , minutes = 0
+                            , seconds = 0
+                            }
+                        )
+        , test "P3Y6M4DT12H30M5S" <|
             \_ ->
                 "P3Y6M4DT12H30M5S"
-                    |> Parser.run duration
+                    |> fromString
                     |> Expect.equal
-                        (Ok
+                        (Just
                             { years = 3
                             , months = 6
                             , days = 4
@@ -23,26 +36,12 @@ suite =
                             , seconds = 5
                             }
                         )
-        , test "P3Y6M2WT12H30M5S" <|
-            \_ ->
-                "P3Y6M2WT12H30M5S"
-                    |> Parser.run duration
-                    |> Expect.equal
-                        (Ok
-                            { years = 3
-                            , months = 6
-                            , days = 14
-                            , hours = 12
-                            , minutes = 30
-                            , seconds = 5
-                            }
-                        )
         , test "P12W" <|
             \_ ->
                 "P12W"
-                    |> Parser.run duration
+                    |> fromString
                     |> Expect.equal
-                        (Ok
+                        (Just
                             { years = 0
                             , months = 0
                             , days = 84
@@ -54,9 +53,9 @@ suite =
         , test "P0.5Y" <|
             \_ ->
                 "P0.5Y"
-                    |> Parser.run duration
+                    |> fromString
                     |> Expect.equal
-                        (Ok
+                        (Just
                             { years = 0.5
                             , months = 0
                             , days = 0
@@ -65,4 +64,119 @@ suite =
                             , seconds = 0
                             }
                         )
+        , test "P0.5Y0.5M" <|
+            \_ ->
+                "P0.5Y0.5M"
+                    |> fromString
+                    |> Expect.equal
+                        (Just
+                            { years = 0.5
+                            , months = 0.5
+                            , days = 0
+                            , hours = 0
+                            , minutes = 0
+                            , seconds = 0
+                            }
+                        )
+        , test "P0,5Y" <|
+            \_ ->
+                "P0,5Y"
+                    |> fromString
+                    |> Expect.equal
+                        (Just
+                            { years = 0.5
+                            , months = 0
+                            , days = 0
+                            , hours = 0
+                            , minutes = 0
+                            , seconds = 0
+                            }
+                        )
+        , test "PT36H" <|
+            \_ ->
+                "PT36H"
+                    |> fromString
+                    |> Expect.equal
+                        (Just
+                            { years = 0
+                            , months = 0
+                            , days = 0
+                            , hours = 36
+                            , minutes = 0
+                            , seconds = 0
+                            }
+                        )
+        , test "P4Y" <|
+            \_ ->
+                "P4Y"
+                    |> fromString
+                    |> Expect.equal
+                        (Just
+                            { years = 4
+                            , months = 0
+                            , days = 0
+                            , hours = 0
+                            , minutes = 0
+                            , seconds = 0
+                            }
+                        )
+        , test "PT1M" <|
+            \_ ->
+                "PT1M"
+                    |> fromString
+                    |> Expect.equal
+                        (Just
+                            { years = 0
+                            , months = 0
+                            , days = 0
+                            , hours = 0
+                            , minutes = 1
+                            , seconds = 0
+                            }
+                        )
+        , test "PT" <|
+            \_ ->
+                "PT"
+                    |> fromString
+                    |> Expect.equal Nothing
+        , test "PD" <|
+            \_ ->
+                "PD"
+                    |> fromString
+                    |> Expect.equal Nothing
+        , test "P1111111111 " <|
+            \_ ->
+                "P1111111111 "
+                    |> fromString
+                    |> Expect.equal Nothing
+        , test "P" <|
+            \_ ->
+                "P"
+                    |> fromString
+                    |> Expect.equal Nothing
+        , test "P12WT12H30M5S" <|
+            \_ ->
+                "P12WT12H30M5S"
+                    |> fromString
+                    |> Expect.equal Nothing
+        , test "P0.5S0.5M" <|
+            \_ ->
+                "P0.5S0.5M"
+                    |> fromString
+                    |> Expect.equal Nothing
+        , test "P0.5A" <|
+            \_ ->
+                "P0.5A"
+                    |> fromString
+                    |> Expect.equal Nothing
+        , test "P5ST" <|
+            \_ ->
+                "P5ST"
+                    |> fromString
+                    |> Expect.equal Nothing
+        , test "P5M10YT" <|
+            \_ ->
+                "P5ST"
+                    |> fromString
+                    |> Expect.equal Nothing
         ]
